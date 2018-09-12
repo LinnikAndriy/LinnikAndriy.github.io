fb1e2f757da710bbab6508bf9fea0a1f835fd3ae
paskalAto <linnik.andriy16@gmail.com>
2018-09-12 15:41:08 +0300

    next step

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
-// $(document).ready(function(){
-//
-// });
+$(document).ready(function(){
+    new WOW().init();
+    $("#main-menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+    $('.main-header__nav').click(function () {
+        let top = $('.sticky').offset().top;
+        $('body,html').animate({
+            scrollTop: top}, 1500);
+    });
+
+
+    window.onscroll = function() {
+        if ($(document).scrollTop() > ($('.skills').offset().top-200)) {
+            $('.chart').easyPieChart({
+                   lineWidth: 8,
+                   scaleLength: 0,
+                   animate: 2000
+            });
+        }
+    }
+});
